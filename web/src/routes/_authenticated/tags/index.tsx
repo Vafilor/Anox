640e3bb358a8c30fa7dc5a6c0792a8f318a0a894
Andrey Melnikov <vafilor@gmail.com>
2024-04-19 17:43:43 -0700

    Update tags index to show pagination and some UI updates

--- web/src/routes/_authenticated/tags/index.tsx
+++ web/src/routes/_authenticated/tags/index.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useCallback } from "react";
 import AnoxApi, { Tag } from "../../../network/api";
 import { createFileRoute, useRouter, } from "@tanstack/react-router";
+import ColorSquare from "../../../components/color-square/color-square";
+import Pagination from "../../../components/pagination/pagination";
 
 interface TagSearch {
     search?: string;
@@ -36,7 +38,8 @@ export const Route = createFileRoute("/_authenticated/tags/")({
 });
 
 function Tags() {
-    const { results, count } = Route.useLoaderData();
+    const { page, search, ordering } = Route.useSearch();
+    const { results, count, ...rest } = Route.useLoaderData();
     const router = useRouter();
 
     const handleCreateTag = useCallback(async (event: FormEvent<HTMLFormElement>) => {
@@ -61,18 +64,56 @@ function Tags() {
         return <div>No Results Tags</div>;
     }
 
+    // TODO timestamps relative to local timezone?
     return (
-        <div>
+        <div className="p-2 overflow-auto">
+            <h1 className="text-3xl text-center font-medium">Tags</h1>
+            <div className="flex justify-center">
+                <Pagination
+                    page={page ?? 1}
+                    pageSize={15}
+                    maxItems={8}
+                    count={count}
+                    className="my-4"
+                />
+            </div>
             <form method="post" onSubmit={handleCreateTag}>
                 <input name="name" placeholder="name" />
                 <input name="color" placeholder="color" />
                 <button type="submit">Create new tag</button>
             </form>
-            {results.map((tag: Tag) => (
-                <div key={tag.id}>
-                    {tag.name}
-                </div>
-            ))}
+            <table className="table-fixed w-full border">
+                <thead>
+                    <tr>
+                        <th className="border p-2 text-left">Name</th>
+                        <th className="border p-2 text-left w-[80px]">Color</th>
+                        <th className="border p-2 text-left">Created</th>
+                        <th className="border p-2 text-left">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {results.map((tag: Tag) => (
+                        <tr key={tag.id}>
+                            <td className="border p-2 ">{tag.name}</td>
+                            <td className="border p-2 ">
+                                <ColorSquare className="ml-2 border border-black" color={tag.color} length={25} />
+                            </td>
+                            <td className="border p-2 ">{tag.createdAt}</td>
+                            <td className="border p-2 ">View</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <div className="flex justify-center">
+                <Pagination
+                    page={page ?? 1}
+                    pageSize={15}
+                    maxItems={8}
+                    count={count}
+                    className="mt-4"
+                />
+            </div>
         </div>
     );
 }
