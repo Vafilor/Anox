24e23cb02d11f37587f52200d5d57d4362e96414
Andrey Melnikov <vafilor@gmail.com>
2024-04-25 16:56:49 -0700

    Add custom box shadow focus class

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        focus: "0 0 0 .25rem rgba(13, 110, 253, .25)"
+      }
+    },
   },
   plugins: [],
 }
