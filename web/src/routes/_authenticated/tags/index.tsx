3f0185d2e293cc510cbaad47a0b78aae0c4eda79
Andrey Melnikov <vafilor@gmail.com>
2024-04-25 17:01:52 -0700

    Rework Tags Index to improve visuals and better handle creating a tag

--- web/src/routes/_authenticated/tags/index.tsx
+++ web/src/routes/_authenticated/tags/index.tsx
@@ -1,13 +1,24 @@
-import { FormEvent, useCallback } from "react";
-import AnoxApi, { Tag } from "../../../network/api";
+import AnoxApi, { Tag, isApiError } from "../../../network/api";
 import { createFileRoute, useRouter, } from "@tanstack/react-router";
 import ColorSquare from "../../../components/color-square/color-square";
 import Pagination from "../../../components/pagination/pagination";
+import { useForm } from "react-hook-form";
+import { DEFAULT_COLOR } from "../../../constants";
+import { useState } from "react";
+import { autoUpdate, offset, useFloating } from '@floating-ui/react';
+import parseApiColor, { clientToApiColor } from "../../../util/color";
+import ErrorText from "../../../components/error/error-text";
+import Button from "../../../components/button/button";
+import ColorPicker from "../../../components/color-picker/color-picker";
 
 interface TagSearch {
     search?: string;
     page?: number;
     ordering?: string;
+}
+
+interface CreateTagInput {
+    name: string;
 }
 
 export const Route = createFileRoute("/_authenticated/tags/")({
@@ -39,26 +50,56 @@ export const Route = createFileRoute("/_authenticated/tags/")({
 
 function Tags() {
     const { page, search, ordering } = Route.useSearch();
-    const { results, count, ...rest } = Route.useLoaderData();
+    const { results, count } = Route.useLoaderData();
     const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { isSubmitting, errors }
+    } = useForm<CreateTagInput>();
 
-    const handleCreateTag = useCallback(async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const [createErrors, setCreateErrors] = useState<string[]>([]);
+    const [color, setColor] = useState(DEFAULT_COLOR);
+    const [colorPickerOpen, setColorPickerOpen] = useState(false);
+    const { refs, floatingStyles } = useFloating({
+        whileElementsMounted: autoUpdate,
+        placement: "bottom-end",
+        middleware: [
+            offset({
+                mainAxis: 5
+            })
+        ]
+    });
 
-        const formData = new FormData(event.currentTarget);
-        const name = formData.get("name") as string;
-        const color = formData.get("color") as string;
+    async function submitCreateTag({ name }: CreateTagInput) {
+        setCreateErrors([]);
 
-        const result = await AnoxApi.createTag({
-            name, color
-        });
+        try {
+            await AnoxApi.createTag({
+                name,
+                color: clientToApiColor(color || DEFAULT_COLOR)
+            });
 
-        console.log(result);
-
-        router.invalidate();
+            reset();
+            setColor(DEFAULT_COLOR);
+            router.invalidate();
+        } catch (err: unknown) {
+            if (isApiError(err)) {
+                setCreateErrors([err.error.message]);
+            } else if (typeof err === "object") {
+                const errors: string[] = [];
+                for (let [key, value] of Object.entries(err as object)) {
+                    errors.push(`${key}: ${value}`);
+                }
+                setCreateErrors(errors);
+            } else {
+                console.error(err);
+            }
+        }
 
         return { name, color };
-    }, [router]);
+    };
 
     if (!results) {
         return <div>No Results Tags</div>;
@@ -66,54 +107,108 @@ function Tags() {
 
     // TODO timestamps relative to local timezone?
     return (
-        <div className="p-2 overflow-auto">
-            <h1 className="text-3xl text-center font-medium">Tags</h1>
-            <div className="flex justify-center">
-                <Pagination
-                    page={page ?? 1}
-                    pageSize={15}
-                    maxItems={8}
-                    count={count}
-                    className="my-4"
-                />
-            </div>
-            <form method="post" onSubmit={handleCreateTag}>
-                <input name="name" placeholder="name" />
-                <input name="color" placeholder="color" />
-                <button type="submit">Create new tag</button>
-            </form>
-            <table className="table-fixed w-full border">
-                <thead>
-                    <tr>
-                        <th className="border p-2 text-left">Name</th>
-                        <th className="border p-2 text-left w-[80px]">Color</th>
-                        <th className="border p-2 text-left">Created</th>
-                        <th className="border p-2 text-left">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {results.map((tag: Tag) => (
-                        <tr key={tag.id}>
-                            <td className="border p-2 ">{tag.name}</td>
-                            <td className="border p-2 ">
-                                <ColorSquare className="ml-2 border border-black" color={tag.color} length={25} />
-                            </td>
-                            <td className="border p-2 ">{tag.createdAt}</td>
-                            <td className="border p-2 ">View</td>
-                        </tr>
+        <>
+            <div className="p-2 overflow-auto">
+                <h1 className="text-3xl text-center font-medium">Tags</h1>
+                <div className="flex justify-center">
+                    <Pagination
+                        page={page ?? 1}
+                        pageSize={15}
+                        maxItems={8}
+                        count={count}
+                        className="my-4"
+                    />
+                </div>
+                <div className="font-semibold text-lg">Add tag</div>
+                <form
+                    method="post"
+                    onSubmit={handleSubmit(submitCreateTag)}
+                    className="w-100">
+                    <div className="flex gap-2 w-100">
+                        <input
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: "Tag must have a name"
+                                }
+                            })}
+                            onInput={() => {
+                                if (createErrors.length) {
+                                    setCreateErrors([]);
+                                }
+                            }}
+                            autoComplete="off"
+                            className="p-2 border rounded w-full grow"
+                            placeholder="tag name"
+                        />
+                        <div className="px-1.5 inline-flex items-center border rounded">
+                            <ColorSquare
+                                ref={refs.setReference}
+                                color={color}
+                                length={25}
+                                className="border border-black"
+                                onClick={() => setColorPickerOpen(true)}
+                            />
+                        </div>
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            disabled={isSubmitting}
+                            loading={isSubmitting}>
+                            Create
+                        </Button>
+                    </div>
+                    <ErrorText>{errors.name?.message}</ErrorText>
+                    {createErrors.map((message, index) => (
+                        <ErrorText key={index}>{message}</ErrorText>
                     ))}
-                </tbody>
-            </table>
+                </form>
+                <table className="table-fixed w-full border mt-2">
+                    <thead>
+                        <tr>
+                            <th className="border p-2 text-left">Name</th>
+                            <th className="border p-2 text-left w-[80px]">Color</th>
+                            <th className="border p-2 text-left">Created</th>
+                            <th className="border p-2 text-left">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {results.map((tag: Tag) => (
+                            <tr key={tag.id}>
+                                <td className="border p-2 ">{tag.name}</td>
+                                <td className="border p-2 ">
+                                    <ColorSquare
+                                        className="ml-2 border border-black"
+                                        color={parseApiColor(tag.color)}
+                                        length={25}
+                                    />
+                                </td>
+                                <td className="border p-2 ">{tag.createdAt}</td>
+                                <td className="border p-2 ">View</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
 
-            <div className="flex justify-center">
-                <Pagination
-                    page={page ?? 1}
-                    pageSize={15}
-                    maxItems={8}
-                    count={count}
-                    className="mt-4"
+                <div className="flex justify-center">
+                    <Pagination
+                        page={page ?? 1}
+                        pageSize={15}
+                        maxItems={8}
+                        count={count}
+                        className="mt-4"
+                    />
+                </div>
+            </div >
+            {colorPickerOpen && (
+                <ColorPicker
+                    ref={refs.setFloating}
+                    color={color}
+                    setColor={setColor}
+                    setOpen={setColorPickerOpen}
+                    style={floatingStyles}
                 />
-            </div>
-        </div>
+            )}
+        </>
     );
 }
