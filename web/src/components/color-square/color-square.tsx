871855f69682b50fda64f4b021cb2c4c7cfdbb4e
Andrey Melnikov <vafilor@gmail.com>
2024-04-26 19:21:12 -0700

    Update ColorSquare to support an optional border  * Rounded border  * Darkens the the color input and sets it as the border

--- web/src/components/color-square/color-square.tsx
+++ web/src/components/color-square/color-square.tsx
@@ -1,23 +1,34 @@
 import { forwardRef } from "react";
-import parseApiColor from "../../util/color";
+import tinycolor from "tinycolor2";
+import classNames from "classnames";
 
 interface Props {
-    color: string; // Hex, RGB or RGBA
+    color: string; // Hex, RRGGBB or RRGGBBAA
     length: number;
     className?: string;
+    border?: boolean;
+    rounded?: boolean;
     onClick?: () => void;
 }
 
 const ColorSquare = forwardRef<HTMLDivElement, Props>(
-    function ColorSquare({ color, length, className, onClick }, ref) {
+    function ColorSquare({ color, length, border, rounded, className, onClick }, ref) {
+        const extraStyles = border ? {
+            borderColor: tinycolor(color).darken(20).toString()
+        } : {};
+
         return (
             <div
                 ref={ref}
-                className={className}
+                className={classNames(className, {
+                    "border": !!border,
+                    "rounded": !!rounded
+                })}
                 style={{
                     width: length,
                     height: length,
-                    backgroundColor: color
+                    backgroundColor: color,
+                    ...extraStyles
                 }}
                 data-color={color}
                 onClick={() => onClick?.()}>
