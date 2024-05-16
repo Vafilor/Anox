e1ee89d2fe6c5f502a3067d3ca7c9ac0190ae6bf
Andrey Melnikov <vafilor@gmail.com>
2024-05-16 10:45:05 -0700

    Added logout button to header

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -2,4 +2,5 @@ export const BASE_URL = 'http://localhost:8000';
 export const BASE_API_URL = BASE_URL + '/api';
 
 export const HOMEPAGE_ROUTE = "/";
+export const LOGIN_ROUTE = "/login";
 export const DEFAULT_COLOR = "#5D5D5D"
--- web/src/routes/_authenticated.tsx
+++ web/src/routes/_authenticated.tsx
@@ -1,6 +1,12 @@
-import { Link, Outlet, createFileRoute, redirect } from "@tanstack/react-router";
+import { Link, Outlet, createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { useDismiss, useFloating, useInteractions, offset } from "@floating-ui/react";
+import { useAuth } from "../auth/authContext";
+import Button from "../components/button/button";
+import { LOGIN_ROUTE } from "../constants";
 
 interface UrlElement {
     name: string;
@@ -50,15 +56,47 @@ export const Route = createFileRoute("/_authenticated")({
 
 function AuthenticatedComponent() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [profileOpen, setProfileOpen] = useState(false);
+    const navigate = useNavigate();
+
+    const { refs, floatingStyles, context } = useFloating({
+        open: profileOpen,
+        onOpenChange: setProfileOpen,
+        placement: "bottom-end",
+        middleware: [
+            offset({
+                mainAxis: 10,
+                crossAxis: -10,
+            })
+        ]
+    });
+
+    const dismiss = useDismiss(context);
+    const { getReferenceProps, getFloatingProps } = useInteractions([dismiss]);
+
+    const auth = useAuth();
+
+    const signOut = () => {
+        auth.signOut();
+        navigate({ to: LOGIN_ROUTE });
+    };
 
     return (
         <>
-            <header className="min-h-[64px] bg-zinc-900 flex items-center">
+            <header className="min-h-[64px] bg-zinc-900 flex items-center justify-between">
                 <button
                     type="button"
-                    className="pl-3 block md:hidden text-white w-[30px] h-[30px]"
+                    className="pl-3 block md:invisible text-white w-[30px] h-[30px]"
                     onClick={() => setSidebarOpen(prev => !prev)}>
                     <HamburgerMenuIcon className="h-full w-full" />
+                </button>
+                <button
+                    ref={refs.setReference}
+                    {...getReferenceProps()}
+                    className="pr-3 flex gap-2 items-center"
+                    onClick={() => setProfileOpen(prev => !prev)}>
+                    <FontAwesomeIcon icon={faCircleUser} size="lg" color="white" />
+                    <FontAwesomeIcon icon={faCaretDown} color="white" />
                 </button>
             </header>
             <div className="flex h-[calc(100vh-64px)]">
@@ -87,6 +125,22 @@ function AuthenticatedComponent() {
                 </div>
                 <Outlet />
             </div>
+            {profileOpen && (
+                <div
+                    ref={refs.setFloating}
+                    {...getFloatingProps()}
+                    style={floatingStyles}
+                    className="bg-zinc-50 border rounded">
+                    <div className="px-3 py-2">{auth.user?.username}</div>
+                    <hr />
+                    <Button
+                        variant="basic"
+                        className=""
+                        onClick={() => signOut()}>
+                        Logout
+                    </Button>
+                </div>
+            )}
         </>
     );
 }
