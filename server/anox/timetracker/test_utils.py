126ceeb2461f822b75228beaba78973637bf1445
Andrey Melnikov <vafilor@gmail.com>
2024-05-21 14:20:27 -0700

    Added date utility functions for splitting dates by day

--- server/anox/timetracker/test_utils.py
+++ server/anox/timetracker/test_utils.py
@@ -1,9 +1,61 @@
+from datetime import datetime, timedelta
 from unittest import TestCase as UnitTestCase
 
-from .utils import to_canonical_name
+from .utils import split_datetimes_across_days, start_of_next_day, to_canonical_name
 
 
 class UtilsTests(UnitTestCase):
     def test_to_canonical_name(self):
         self.assertEqual(to_canonical_name("CaPitAl"), "capital")
         self.assertEqual(to_canonical_name("  spa ce   "), "spa ce")
+
+    def test_start_of_next_day(self):
+        when = datetime(year=2000, month=11, day=23)
+        next = start_of_next_day(when=when)
+        self.assertEqual(next.day, 24)
+        self.assertEqual(next.month, 11)
+
+        when = datetime(year=2000, month=11, day=30)
+        next = start_of_next_day(when=when)
+        self.assertEqual(next.day, 1)
+        self.assertEqual(next.month, 12)
+
+        when = datetime(year=2000, month=12, day=31)
+        next = start_of_next_day(when=when)
+        self.assertEqual(next.day, 1)
+        self.assertEqual(next.month, 1)
+        self.assertEqual(next.year, 2001)
+
+    def test_split_datetimes_across_days_same_day(self):
+        start = datetime(year=2000, month=11, day=23)
+        end = start + timedelta(hours=1)
+
+        parts = split_datetimes_across_days(start=start, end=end)
+
+        self.assertEqual(len(parts), 2)
+        self.assertEqual(parts[0].day, 23)
+        self.assertEqual(parts[1].day, 23)
+
+    def test_split_datetimes_across_days_exception(self):
+        start = datetime(year=2000, month=11, day=23)
+        end = start - timedelta(days=1)
+
+        with self.assertRaises(ValueError):
+            split_datetimes_across_days(start=start, end=end)
+
+    def test_split_datetimes_across_days(self):
+        start = datetime(year=2000, month=11, day=23)
+        end = datetime(year=2000, month=11, day=25, hour=13)
+
+        parts = split_datetimes_across_days(start=start, end=end)
+
+        self.assertEqual(len(parts), 6)
+
+        self.assertEqual(parts[0].day, 23)
+        self.assertEqual(parts[1].day, 24)
+
+        self.assertEqual(parts[2].day, 24)
+        self.assertEqual(parts[3].day, 25)
+
+        self.assertEqual(parts[4].day, 25)
+        self.assertEqual(parts[5].day, 25)
--- server/anox/timetracker/utils.py
+++ server/anox/timetracker/utils.py
@@ -1,2 +1,52 @@
+from datetime import datetime, timedelta
+
+
 def to_canonical_name(name: str) -> str:
     return name.lower().strip()
+
+
+def start_of_next_day(when: datetime) -> datetime:
+    next = when + timedelta(days=1)
+
+    return next.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def split_datetimes_across_days(start: datetime, end: datetime) -> list[datetime]:
+    """
+    Given a start and end datetime, return a list of datetimes
+    that split the input dates by the day.
+
+    If the inputs are:
+        start: 1/1/2000 8:35
+        end: 1/4/2000 7:23
+
+    the resulting list will have (with hours/min/seconds/microseconds
+    kept for start/end dates and 0 for others)
+    [
+        1/1/2000 8:35, 1/2/2000 0:00,
+        1/2/2000 0:00, 1/3/2000 0:00,
+        1/3/2000 0:00, 1/4/2000 7:23
+    ]
+    """
+    if start.day == end.day:
+        return [start, end]
+
+    if end.day < start.day:
+        raise ValueError(f"End day ({end.day}) is before start day ({start.day})")
+
+    result = []
+
+    mid = start_of_next_day(start)
+    result.append(start)
+    result.append(mid)
+
+    while mid.day < end.day:
+        next = start_of_next_day(mid)
+        result.append(mid)
+        result.append(next)
+        mid = next
+
+    result.append(mid)
+    result.append(end)
+
+    return result
